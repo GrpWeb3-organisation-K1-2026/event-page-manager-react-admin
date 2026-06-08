1cea0e18aa2d685d03ab20d07262797f58568cca
liantsoaaa <ramaharsionl@gmail.com>
2026-06-08 12:23:16 +0300

    feat(admin): complete App.tsx and add SessionEdit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-// src/App.tsx
 import { Admin, Resource } from "react-admin";
 import { Layout } from "./Layout";
 
 import { EventList } from "./resources/events/EventList";
 import { EventCreate } from "./resources/events/EventCreate";
 import { EventEdit } from "./resources/events/EventEdit";
+
+import { SessionList } from "./resources/sessions/SessionList";
+import { SessionCreate } from "./resources/sessions/SessionCreate";
+import { SessionEdit } from "./resources/sessions/SessionEdit";
+
+import { SpeakerList } from "./resources/speakers/SpeakerList";
+import { SpeakerCreate } from "./resources/speakers/SpeakerCreate";
+import { SpeakerEdit } from "./resources/speakers/SpeakerEdit";
 
 import { RoomList } from "./resources/rooms/RoomList";
 import { RoomCreate } from "./resources/rooms/RoomCreate";
@@ -13,13 +20,14 @@ import { RoomEdit } from "./resources/rooms/RoomEdit";
 import { QuestionList } from "./resources/questions/QuestionList";
 
 import EventIcon from "@mui/icons-material/Event";
+import GroupIcon from "@mui/icons-material/Group";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import VideoLabelIcon from "@mui/icons-material/VideoLabel";
 import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
 
 export const App = () => (
   <Admin
     layout={Layout}
-
     title="EventSync Admin"
   >
     <Resource
@@ -29,6 +37,22 @@ export const App = () => (
       create={EventCreate}
       edit={EventEdit}
       options={{ label: "Events" }}
+    />
+    <Resource
+      name="sessions"
+      icon={VideoLabelIcon}
+      list={SessionList}
+      create={SessionCreate}
+      edit={SessionEdit}
+      options={{ label: "Sessions" }}
+    />
+    <Resource
+      name="speakers"
+      icon={GroupIcon}
+      list={SpeakerList}
+      create={SpeakerCreate}
+      edit={SpeakerEdit}
+      options={{ label: "Speakers" }}
     />
     <Resource
       name="rooms"
