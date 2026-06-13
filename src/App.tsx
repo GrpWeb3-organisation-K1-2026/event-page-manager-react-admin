a8dec04f8e1c27503e6cb025fcc4a2ed9f14f964
Miranto <hei.finoana.5@gmail.com>
2026-06-13 05:16:38 +0300

    feat: add authProvider

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { SessionList } from "./resources/sessions/SessionList";
 import { SessionCreate } from "./resources/sessions/SessionCreate";
 import { SessionEdit } from "./resources/sessions/SessionEdit";
 
-import { SpeakerList } from "./resources/speakers/SpeakerList";
+import { SpeakerList } from "./resources/speakers/SpeakersList";
 import { SpeakerCreate } from "./resources/speakers/SpeakerCreate";
 import { SpeakerEdit } from "./resources/speakers/SpeakerEdit";
 
@@ -24,9 +24,11 @@ import GroupIcon from "@mui/icons-material/Group";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
 import VideoLabelIcon from "@mui/icons-material/VideoLabel";
 import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
+import { authProvider } from "./authProvider";
 
 export const App = () => (
   <Admin
+  authProvider={authProvider}
     layout={Layout}
     title="EventSync Admin"
   >
